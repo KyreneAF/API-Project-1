4fe86fa116720860e8f2023afc967199f9dce694
Kyrene <Kyrene.Aflores@gmail.com>
2023-10-31 23:00:45 -0500

    maybe fixed dates...not sure need to merge to main and test

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       })
     }
   }
+
   Booking.init({
     spotId: {
       type:DataTypes.INTEGER,
@@ -40,8 +41,43 @@ module.exports = (sequelize, DataTypes) => {
     endDate: {
       type:DataTypes.DATE,
       allowNull:false,
-    }
-  }, {
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      get() {
+        const createdAt = this.getDataValue('createdAt');
+        if (createdAt) {
+          const year = createdAt.getFullYear();
+          const month = String(createdAt.getMonth());
+          const day = String(createdAt.getDate());
+          const hours = String(createdAt.getHours());
+          const minutes = String(createdAt.getMinutes());
+          const seconds = String(createdAt.getSeconds());
+          return year + '-' + month + '-' + day +' '+hours +':'+minutes +':'+seconds
+        } else {
+          return null;
+        }
+      },
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      get() {
+        const updatedAt = this.getDataValue('updatedAt');
+        if (updatedAt) {
+          const year = updatedAt.getFullYear();
+          const month = String(updatedAt.getMonth());
+          const day = String(updatedAt.getDate());
+          const hours = String(updatedAt.getHours());
+          const minutes = String(updatedAt.getMinutes());
+          const seconds = String(updatedAt.getSeconds());
+          return year + '-' + month + '-' + day +' '+hours +':'+minutes +':'+seconds
+        } else {
+          return null;
+        }
+      },
+    },
+  },
+  {
     sequelize,
     modelName: 'Booking',
   });
