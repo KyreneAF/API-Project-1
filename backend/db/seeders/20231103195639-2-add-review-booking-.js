908621fbde59c8a8cb1515fe6dd5ac15e730973a
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-03 17:26:00 -0500

    finished additional booking seeds

--- backend/db/seeders/20231103195639-2-add-review-booking-.js
+++ backend/db/seeders/20231103195639-2-add-review-booking-.js
@@ -1,25 +1,58 @@
 'use strict';
+const {Booking} = require('../models');
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
+let bookingArr =[
+  {
+    spotId:7,
+    userId:8,
+    startDate:new Date ('2025-02-15'),
+    endDate:new Date ('2025-02-17')
+  },
+  {
+    spotId:9,
+    userId:1,
+    startDate: new Date ('2025-10-30'),
+    endDate:new Date ('2025-10-31')
+  },
+  {
+    spotId:10,
+    userId:7,
+    startDate: new Date ('2024-12-23'),
+    endDate:new Date ('2024-12-26')
+  },
+  {
+    spotId:4,
+    userId:1,
+    startDate: new Date ('2025-11-11'),
+    endDate:new Date ('2025-11-12')
+  },
+  {
+    spotId:2,
+    userId:5,
+    startDate: new Date ('2025-01-01'),
+    endDate:new Date ('2025-01-05')
+  },
+
+]
+
+
+
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Booking.bulkCreate(bookingArr,{validate:true })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options,{
+      spotId:{[Op.in]:bookingArr.map(book => book.spotId)}
+    },{})
   }
 };
