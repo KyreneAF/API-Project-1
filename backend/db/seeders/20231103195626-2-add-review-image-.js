1db774055e8f85d2de17be7cbd0dcee9c9362501
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-03 17:13:30 -0500

    finished add review image

--- backend/db/seeders/20231103195626-2-add-review-image-.js
+++ backend/db/seeders/20231103195626-2-add-review-image-.js
@@ -1,6 +1,34 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+const {ReviewImage} = require('../models');
+let options = {};
+if(process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA;
+}
+// /** @type {import('sequelize-cli').Migration} */
+let reviewImgArr =[
+  {
+    reviewId:1,
+    url:'review url image 6',
+  },
+  {
+    reviewId:2,
+    url:'review url image 7',
+  },
+  {
+    reviewId:3,
+    url:'review url image 8',
+  },
+  {
+    reviewId:4,
+    url:'review url image 9',
+  },
+  {
+    reviewId:5,
+    url:'review url image 10',
+  },
+]
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
