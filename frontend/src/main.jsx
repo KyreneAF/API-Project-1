b07b5b114fd75ea854007a71757fb96d38993a6f
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-17 13:00:04 -0600

    finished setting up seesios frontend still an issue with server

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+// import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
-
-//  NOT SURE WHICH TO USE THIS IS IN THE READING THIS ONE OR THE ONE BELOW
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
-// if (import.meta.env.MODE !== 'production') {
-//   window.store = store;
-// }
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
     </Provider>
   </React.StrictMode>
 );
