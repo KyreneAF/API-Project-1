4f832c64a2ff2091f571e4cad3f294558486a07e
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-17 13:32:44 -0600

    navigation

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfileButton = () => {
+  return (
+    <div style={{ color: 'orange', fontSize: '24px' }}>
+      {/* Assuming you choose the user icon from Font Awesome */}
+      <i class="fa-solid fa-user"></i>
+    </div>
+  );
+};
+
+export default ProfileButton;
