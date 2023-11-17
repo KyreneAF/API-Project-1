1b7ee9259f3ca3334348f5be23b97fd393a7df95
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-17 12:07:00 -0600

    created frontend csrf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,28 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
-
+//  NOT SURE WHICH TO USE THIS IS IN THE READING THIS ONE OR THE ONE BELOW
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
+
+// if (import.meta.env.MODE !== 'production') {
+//   window.store = store;
+// }
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
