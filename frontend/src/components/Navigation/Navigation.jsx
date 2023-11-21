448123db182cb0491a7207c0b28c40199400ef4c
Kyrene <Kyrene.Aflores@gmail.com>
2023-11-21 15:46:47 -0600

    fixed render issues

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -55,111 +55,111 @@
 
 // THIS IS DIFFERENT STYLING TOP IS ORIGINAL
 
-// import { NavLink } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import ProfileButton from './ProfileButton';
-// import './Navigation.css';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
-// function Navigation({ isLoaded }) {
-//   const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
 
-//   return (
-//     <ul>
-//       <li>
-//         <NavLink exact to="/">Home</NavLink>
-//       </li>
-//       {isLoaded && (
-//         <li>
-//           <ProfileButton user={sessionUser} />
-//         </li>
-//       )}
-//     </ul>
-//   );
-// }
+  return (
+    <div className=''>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
+    </div>
+  );
+}
 
-// export default Navigation;
+export default Navigation;
 
 
 
 
 // OPTION 3 THIS IS CONNECTED TO OpenModalMenuItem
 
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
-import OpenModalMenuItem from './OpenModalMenuItem';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
-import SignupFormModal from '../SignupFormModal/SignupFormModal';
+// import { useState, useEffect, useRef } from 'react';
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+// import OpenModalMenuItem from './OpenModalMenuItem';
+// import LoginFormModal from '../LoginFormModal/LoginFormModal';
+// import SignupFormModal from '../SignupFormModal/SignupFormModal';
 
-function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
+// function ProfileButton({ user }) {
+//   const dispatch = useDispatch();
+//   const [showMenu, setShowMenu] = useState(false);
+//   const ulRef = useRef();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
-  };
+//   const toggleMenu = (e) => {
+//     e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+//     setShowMenu(!showMenu);
+//   };
 
-  useEffect(() => {
-    if (!showMenu) return;
+//   useEffect(() => {
+//     if (!showMenu) return;
 
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
-    };
+//     const closeMenu = (e) => {
+//       if (!ulRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
 
-    document.addEventListener('click', closeMenu);
+//     document.addEventListener('click', closeMenu);
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+//     return () => document.removeEventListener("click", closeMenu);
+//   }, [showMenu]);
 
-  const closeMenu = () => setShowMenu(false);
+//   const closeMenu = () => setShowMenu(false);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-    closeMenu();
-  };
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//     closeMenu();
+//   };
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
-  return (
-    <>
-      <button onClick={toggleMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
-      <ul className={ulClassName} ref={ulRef}>
-        {user ? (
-          <>
-            <li>{user.username}</li>
-            <li>{user.firstName} {user.lastName}</li>
-            <li>{user.email}</li>
-            <li>
-              <button onClick={logout}>Log Out</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <OpenModalMenuItem
-              itemText="Log In"
-              onItemClick={closeMenu}
-              modalComponent={<LoginFormModal />}
-            />
-            <OpenModalMenuItem
-              itemText="Sign Up"
-              onItemClick={closeMenu}
-              modalComponent={<SignupFormModal />}
-            />
-          </>
-        )}
-      </ul>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <button onClick={toggleMenu}>
+//         <i className="fas fa-user-circle" />
+//       </button>
+//       <ul className={ulClassName} ref={ulRef}>
+//         {user ? (
+//           <>
+//             <li>{user.username}</li>
+//             <li>{user.firstName} {user.lastName}</li>
+//             <li>{user.email}</li>
+//             <li>
+//               <button onClick={logout}>Log Out</button>
+//             </li>
+//           </>
+//         ) : (
+//           <>
+//             <OpenModalMenuItem
+//               itemText="Log In"
+//               onItemClick={closeMenu}
+//               modalComponent={<LoginFormModal />}
+//             />
+//             <OpenModalMenuItem
+//               itemText="Sign Up"
+//               onItemClick={closeMenu}
+//               modalComponent={<SignupFormModal />}
+//             />
+//           </>
+//         )}
+//       </ul>
+//     </>
+//   );
+// }
 
-export default ProfileButton;
+// export default ProfileButton;
 
 // import { useState, useEffect, useRef } from 'react';
 // import { useDispatch } from 'react-redux';
