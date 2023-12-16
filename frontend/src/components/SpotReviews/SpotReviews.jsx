d48d3baea53cccafce1be965a481440b3ab9eede
Kyrene <Kyrene.Aflores@gmail.com>
2023-12-16 14:06:00 -0600

    fixed errors to run build

--- frontend/src/components/SpotReviews/SpotReviews.jsx
+++ frontend/src/components/SpotReviews/SpotReviews.jsx
@@ -21,12 +21,12 @@ export function SpotReviews({spotId, ownerId, avgRating, numReviews}){
 
 
   useEffect(() => {
-    const res = dispatch(thunkGetReviews(spotId))
+    dispatch(thunkGetReviews(spotId))
     // if(!res.ok){
 
     //   console.log("issue with useEffect dispatch")
     // }
-  })
+  },[spotId,dispatch])
 
 
 
@@ -65,7 +65,7 @@ return(
     <div className='review-list-container' >
       {
       reviewsArr.map(review =>(
-        <div className='indie-review-container'>
+        <div className='indie-review-container' key={review.id}>
         <p>{review.User.firstName}</p>
         <div>{dateFormater({review})}</div>
         <p>{review.review}</p>
@@ -79,91 +79,3 @@ return(
   </>
 
 )}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect, useState } from "react";
-// import { thunkGetReviews } from "../../store/reviews";
-// import { dateFormater } from "./SpotReviewFuncs";
-// import { useParams } from "react-router-dom";
-// import OpenModalButton from "../OpenModalButton/OpenModalButton";
-// import { CreateReview } from "../CreateReview/CreateReview";
-
-// export const SpotReviews = () => {
-//   const { id } = useParams();
-//   const dispatch = useDispatch();
-//   const [errors, setErrors] = useState({});
-
-//   const spotReview = useSelector((state) => state.reviews);
-//   const currSpot = useSelector((state) => state.spots.spot);
-//   const currUser = useSelector((state) => state.session.user);
-//   //   console.log("this is spotReview", spotReview);
-//   // console.log('!!!',currSpot.ownerId,currUser)
-
-//   useEffect(() => {
-//     dispatch(thunkGetReviews(id));
-//   }, [dispatch, id]);
-
-
-//   useEffect(() => {
-//     const errObj = {};
-
-//     if (currSpot.ownerId === currUser.id) errObj.same = "same user";
-
-//     setErrors(errObj);
-//   }, [currSpot.ownerId, currUser.id]);
-
-//   return (
-//     <>
-//       <div className="post-review-bttn">
-//         {!errors.same && (
-//           <OpenModalButton
-//             buttonText="Post Your Review"
-//             modalComponent={<CreateReview />}
-//           />
-//         )}
-//       </div>
-//       {!spotReview.reviews ? (
-//         <div>Be the first to post a Review!</div>
-//       ) : (
-//         <>
-//           <div className="reviews-main-container">
-//             <div className="indie-review-container">
-//               {spotReview.reviews &&
-//                 spotReview.reviews.map((review) => (
-//                   // spotReview.reviews.map((review) => (
-//                   <div key={review.id}>
-//                     <div className="first-name">{review.User.firstName} </div>
-//                     <div style={{ color: "gray" }}>{dateFormater(review)}</div>
-//                     <div>{review.review}</div>
-//                   </div>
-//                 ))}
-//             </div>
-//           </div>
-//         </>
-//       )}
-//     </>
-//   );
-// };
