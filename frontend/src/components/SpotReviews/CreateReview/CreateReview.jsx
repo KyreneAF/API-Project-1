3bb76a490e3c7619e679ddc692e0fc0bbc2afaea
Kyrene <Kyrene.Aflores@gmail.com>
2023-12-29 17:56:31 -0600

    quick cleanup of ol code to declutter

--- frontend/src/components/SpotReviews/CreateReview/CreateReview.jsx
+++ frontend/src/components/SpotReviews/CreateReview/CreateReview.jsx
@@ -6,14 +6,13 @@ import { thunkGetReviews } from "../../../store/reviews.js";
 import { thunkGetDetailsSpot } from "../../../store/spots.js";
 import { useModal } from "../../../context/Modal.jsx";
 import "./CreateReview.css";
-// import './OpenModalButton/OpenModalButton.css'
+
 
 export const CreateReview = ({ spotId }) => {
-  // const {id}  = useParams(); // <- this seems to not work bc there isn't a route. maybe findSpot instead?
   spotId = Number(spotId);
   const dispatch = useDispatch();
 
-  // const reviews = useSelector(state => state.reviews)
+
   const currUser = useSelector((state) => state.session.user);
   const currSpot = useSelector((state) => state.spots);
   const reviews = useSelector((state) => state.reviews);
@@ -22,7 +21,6 @@ export const CreateReview = ({ spotId }) => {
   const [review, setReview] = useState("");
   const [stars, setStars] = useState("");
   const [errors, setErrors] = useState({});
-  // const maxStars = [1, 2, 3, 4, 5];
 
   console.log("REVIEWS IN CREATE REVIEWS", reviews);
 
@@ -53,7 +51,7 @@ export const CreateReview = ({ spotId }) => {
       review,
       stars,
     };
-    // console.log('CREATED REVIEW', createdReview)
+
 
 
 
@@ -69,7 +67,7 @@ export const CreateReview = ({ spotId }) => {
     dispatch(thunkGetReviews(spotId));
     dispatch(thunkGetDetailsSpot(spotId));
 
-    // return () => dispatch(thunkGetReviews(spotId))
+
   };
 console.log('ERRORS',errors,'STARS',stars)
 
@@ -108,7 +106,7 @@ console.log('ERRORS',errors,'STARS',stars)
 
         <button
           className='review-submit-btn'
-          // disabled={review.length < 10 || stars === ""}
+          disabled={review.length < 10 || stars === ""}
         >
           Submit your review
         </button>
@@ -116,130 +114,3 @@ console.log('ERRORS',errors,'STARS',stars)
     </div>
   );
 };
-
-// OLD CODE REFACTORING ON TOP
-// export const CreateReview = ({spotId }) => {
-//   // const {id}  = useParams(); // <- this seems to not work bc there isn't a route. maybe findSpot instead?
-//    spotId =  Number(spotId)
-//   const dispatch = useDispatch();
-
-//   // const reviews = useSelector(state => state.reviews)
-
-//   const [review, setReview] = useState("");
-//   // const [validations, setValidations] = useState({});
-//   const { closeModal } = useModal();
-//   const [stars, setStars] = useState(0);
-//   const [errors, setErrors] = useState({})
-//   const maxStars = [1, 2, 3, 4, 5];
-
-// //   const [blackStars, setBlackStars] = useState(0)
-
-//     const currUser = useSelector((state) => state.session.user)
-//     const currSpot = useSelector((state) => state.spots)
-//     const reviews = useSelector((state) => state.reviews)
-
-//     console.log('REVIEWS IN CREATE REVIEWS',reviews)
-
-//     const user = {
-//         id:currUser.id,
-//         firstName:currUser.firstName,
-//         lastName:currUser.lastName
-//     }
-
-//   const onSubmit = async (e) => {
-//     e.preventDefault();
-//     // const errObj = {};
-//     const errObj = {};
-
-//     if (review.length < 10) {
-//       errObj.review = "Review must be at least 10 characters";
-
-//     }
-//     if (stars === 0) {
-//       errObj.stars = "Must have at least 1 star!";
-
-//     }
-//     if(currUser.id === currSpot.ownerId ) errObj.same = "same user"
-
-//     setErrors(errObj);
-
-//     const createdReview = {
-//       userId:user.id,
-//       spotId:spotId,
-//       review,
-//       stars,
-//     }
-//     // console.log('CREATED REVIEW', createdReview)
-
-//     if(Object.values(errors).length === 0){
-//       console.log('ERRORS', errors)
-//       await dispatch(thunkCreateReview(spotId, user, createdReview));
-
-//     }
-
-//     setReview("");
-//     setStars(0);
-//     closeModal();
-
-//     dispatch(thunkGetReviews(spotId))
-//     dispatch(thunkGetDetailsSpot(spotId))
-
-//         // return () => dispatch(thunkGetReviews(spotId))
-
-//   };
-
-//   return (
-//     <>
-
-//       <div className="create-review-container">
-//         <div className='cr-title-cont' >How was your stay?</div>
-//         { Object.keys(errors) && <div >{Object.values(errors).map((msg,index) => <div key={index}>{msg}</div>)}</div>}
-
-//         <form className='form-cr-cont' onSubmit={onSubmit}>
-//           <textarea
-//             placeholder="Leave your review here..."
-//             value={review}
-//             onChange={(e) => setReview(e.target.value)}
-//           />
-
-//           <div className="star-container">
-//             {maxStars.map(
-//               (
-//                 star,
-//                 index //<- only way I can think to add a unique key
-//               ) => (
-//                 <label key='index'>
-//                 <input
-//                   key={index}
-//                   type="radio"
-//                   name="stars"
-//                   value={star}
-//                   min={1}
-//                   max={5}
-//                   id={`star-${star}`}
-//                   checked={stars === star}
-//                   onChange={(e) => setStars(Number(e.target.value))}
-//                 />
-//                 {/* <i className="fa-light fa-star"></i> */}
-//                 </label>
-
-//               )
-//             )}
-//             Stars
-//           </div>
-//           <div>
-//             <button
-//             disabled={stars <= 0 || review.length < 10}
-//               type="submit"
-//               className="review-submit-btn"
-//               // disabled={validations.review || validations.stars || errors.same}
-//             >
-//               Submit Your Review
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-
-//     </>
-//   );
-// };
